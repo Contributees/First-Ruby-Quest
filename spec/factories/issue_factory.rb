7910b40e0293166e375165e05654542e6bf7fe4f
Claire Lin <chunhuilin7516@gmail.com>
2023-11-27 20:58:12 +0100

    Add an issue_factory to be used in test for mocking issues

---
+++ spec/factories/issue_factory.rb
@@ -0,0 +1,11 @@
+CODES = ("AAAA".."ZZZZ").to_a.shuffle
+FactoryBot.define do
+  factory :issue, class: Issue do
+    title { "rendering ERB Templates with correct spacing" }
+    category { "open-source" }
+    url {CODES.pop}
+    repo_name {"test-repo"}
+    gh_url {"/"}
+    gh_id { 1234 }
+  end
+end
