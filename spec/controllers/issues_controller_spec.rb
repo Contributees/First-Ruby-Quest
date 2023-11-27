ab2f52090f5fde5c6893f0e10a31a10f07ea89ac
Claire Lin <chunhuilin7516@gmail.com>
2023-11-27 20:58:47 +0100

    Add initial IssuesController tests for the #index happy path

---
+++ spec/controllers/issues_controller_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+RSpec.describe IssuesController, :type => :controller do
+  describe "#index" do
+    let!(:issue_1) {create(:issue)}
+    let!(:issue_2) {create(:issue)}
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    context "without any additional params" do
+      it "loads all of the issues into @issues" do
+        get :index
+        expect(assigns(:issues)).to match_array([issue_1, issue_2])
+      end
+    end
+  end
+
+  describe "#show" do
+  end
+
+  describe "#new" do
+  end
+
+  describe "#create" do
+  end
+end
