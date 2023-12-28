9ee58ecb2552ccc134bf70ec608dbdc68dca626a
Claire Lin <chunhuilin7516@gmail.com>
2023-12-28 19:01:44 +0100

    Add user and tag associations to issue factory

--- spec/factories/issue_factory.rb
+++ spec/factories/issue_factory.rb
@@ -1,11 +1,18 @@
 CODES = ("AAAA".."ZZZZ").to_a.shuffle
 FactoryBot.define do
   factory :issue, class: Issue do
+    association :user, factory: :user
     title { "rendering ERB Templates with correct spacing" }
     category { "open-source" }
     url { CODES.pop }
     repo_name { "test-repo" }
     gh_url { "/" }
     gh_id { 1234 }
+    description { "Veniam rerum nesciunt. Quibusdam quasi non. Est id minima." }
+    assigned { false }
+
+    after(:create) do |issue|
+      create_list(:tag, 2, issues: [issue])
+    end
   end
 end
